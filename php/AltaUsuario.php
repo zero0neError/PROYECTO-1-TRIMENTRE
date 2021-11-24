5cc3246fe431f4ec58cede6a5804b1d8904d247f
Guillermo Rueda Padilla <gruepad414@g.educaand.es>
2021-11-24 08:14:05 +0100

    b

---
+++ php/AltaUsuario.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo 
+    "
+    
+    ";
+    ?>
+    <form action=''>
+        <label for=''></label>
+        <input type="text" name="" id="">
+
+        <label for=""></label>
+        <input type="text" name="" id="">
+
+        <label for=""></label>
+        <input type="text" name="" id="">
+
+        <label for=""></label>
+        <input type="text" name="" id="">
+
+        <label for=""></label>
+        <input type="text" name="" id="">
+
+        <label for=""></label>
+        <input type="time" name="" id="">
+
+        <label for=""></label>
+        <select name="" id="">
+
+        </select>
+    </form>
+</body>
+</html>
