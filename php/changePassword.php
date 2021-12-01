73c06e5cf05e4a4de498e50404f2e2cb4b4b2cc9
Guillermo Rueda Padilla <gruepad414@g.educaand.es>
2021-12-01 09:45:04 +0100

    senpaiiiiiiiiiiiiii

--- php/changePassword.php
+++ php/changePassword.php
@@ -1,4 +1,31 @@
 <?php
+    include_once "../clases/Sesion.php";
+    //Comprueba si el usuario esta logeado, aqui no tiene sentido porque no hace falta estar logeado para estar aqui, pero si hace falta que se haya accedido desde la venta de usuario  
+    // Sesion::init();
+    // if(!Sesion::existe("alumno")){
+
+    //     header("Location: Login.php");
+    // }
+    if(isset($_GET['id']) && $_GET['id']!=""){
+
+        if(isset($_POST['Enviar']) && $_POST['password']!="" && $_POST['repeatPassword']!=""){
+
+            $pass1=$_POST['password'];
+            $pass2=$_POST['repeatPassword'];
+
+            if($pass1!=$pass2){
+
+                echo "<p>Las contraseñas no coinciden</p>";
+            }else{
+
+                //update en vez de con el correo con el hash del md5
+            }
+        }
+        
+    }else{
+
+        header("Location: Login.php");
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -6,11 +33,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../css/changePassword.css">
+
+    <title>Cambiar contraseña</title>
 </head>
 <body>
-    <p></p>
-    <input type="text" name="password" id="txtPassword">
-    <input type="submit" value="Cambiar contraseña">
+    <form action="ChangePassword" method="post">
+        <p>Introduce tu nueva contraseña</p>
+        <input type="text" name="password" id="txtPassword">
+        <p>Repite tu nueva contraseña</p>
+        <input type="text" name="repeatPassword" id="txtRepeatPassword"><br>
+        <input type="submit" name="Enviar" value="Cambiar contraseña">
+    </form>
 </body>
 </html>
---
+++ php/enviaCorreo.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cambiar contraseña</title>
+</head>
+<body>
+    <?php
+        if(isset($_POST['Enviar']) && $_POST['correo']!=""){
+
+            include_once "../php/BD.php";
+            if(BD::conectar()){
+
+                if(BD::existeCorreo($_POST['correo'])){
+
+                    $hash=md5($_POST['correo']);
+                    Email::mandaEmail($_POST['correo'],"Cambiar contraseña","<a href='http://localhost/PROYECTO_PRIMER_TRIMESTRE/php/ChangePassword.php?id=${hash}'>Pulsa para cambiar tu contraseña</a>",null);
+                }
+            }
+            
+        }
+    ?>
+    
+    <form action="" method="post">
+        <p>Introduce tu direccion de email</p>
+        <input type="text" name="correo" id="correo"><br>
+        <input type="submit" value="Enviar" name="Enviar">
+    </form>
+    
+</body>
+</html>
