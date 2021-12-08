81f43da73799252412b9590d1fe27f9d020e0a08
Guillermo Rueda Padilla <gruepad414@g.educaand.es>
2021-12-08 13:44:55 +0100

    a

---
+++ php/PaginaPrincipal.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/menudesplegable.css">
+    <title>Autoescuela los panchos</title>
+</head>
+<body>
+    <img src="../imagenes/pipo.jpg" height="150px" width="1460px" alt="hamster">
+    <nav id="menu">
+    <ul>
+        <li><a href="#">Usuarios</a></li>
+        <li><a href="#">Tematicas</a>
+        <!-- start menu desplegable -->
+            <ul>
+            <li><a href="#">Alta tematica</a></li>
+            </ul>
+        <!-- end menu desplegable -->
+        </li>
+        <li><a href="#">Preguntas</a></li>
+        <li><a href="#">Examenes</a></li>  
+    </ul>
+    </nav>
+</body>
+</html>
